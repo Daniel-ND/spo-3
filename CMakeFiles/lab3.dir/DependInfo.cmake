114c2ecd8a6414cf0c25b5b9337e5177a2466223
Daria Nikolskaia <51816130+Daniel-ND@users.noreply.github.com>
2021-11-30 23:08:19 +0300

    Add files via upload

---
+++ CMakeFiles/lab3.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/aleksey/study/SPO/spo_lab_3/Client/client.c" "/home/aleksey/study/SPO/spo_lab_3/CMakeFiles/lab3.dir/Client/client.c.o"
+  "/home/aleksey/study/SPO/spo_lab_3/Server/config_worker.c" "/home/aleksey/study/SPO/spo_lab_3/CMakeFiles/lab3.dir/Server/config_worker.c.o"
+  "/home/aleksey/study/SPO/spo_lab_3/Server/server.c" "/home/aleksey/study/SPO/spo_lab_3/CMakeFiles/lab3.dir/Server/server.c.o"
+  "/home/aleksey/study/SPO/spo_lab_3/main.c" "/home/aleksey/study/SPO/spo_lab_3/CMakeFiles/lab3.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
